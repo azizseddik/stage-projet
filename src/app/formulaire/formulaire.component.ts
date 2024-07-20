fcdd385213517d0c9375fd54f231c5df6ef1e727
Baga6312 <magroubertn@gmail.com>
2024-07-20 21:20:30 +0100

    fixes

---
+++ src/app/formulaire/formulaire.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormGroup , FormsModule, NgForm} from '@angular/forms';
+import { ArrayModel } from './array';
+
+@Component({
+  selector: 'app-formulaire',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './formulaire.component.html',
+  styleUrl: './formulaire.component.css'
+})
+
+export class FormulaireComponent {
+  reactiveForm: FormGroup = new FormGroup({});
+  public form: FormGroup = new FormGroup([]);
+  public registreArray: ArrayModel;
+  constructor(private retour: Router) {
+    this.registersArray = new ArrayModel();
+  }
+
+  passwordPtn = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,16}$';
+  registersArray: any = { password: '', passwordConfirme: '' };
+  validate() {
+    this.passwordValidation.submitted = true;
+    if (this.isPasswordValid()) {
+      this.retour.navigate(['/projet']);
+    }
+  }
+  passwordValidation = { submitted: false };
+  isPasswordValid(): boolean {
+    return (
+      this.registersArray.password === this.registersArray.passwordConfirme
+    );
+  }
+
+  onSubmit(form:NgForm) {
+    if (form.valid) {
+      console.log(this.registersArray);
+      this.retour.navigate(['/projet']);
+    }
+  }
+  pwdSubmit() {
+    alert(this.registersArray);
+  }
+}
